95996444c3ba94aa19e2fe8b04e5faf858f82460
Joseph Gallardo <josephtk18@gmail.com>
2022-03-18 17:34:17 -0500

    Automatic Scraping from post and JSON files

--- Challenge2_Instagram/InstagramScraper.py
+++ Challenge2_Instagram/InstagramScraper.py
@@ -1,7 +1,12 @@
+from ast import While
+from asyncio.windows_events import NULL
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
 
+#This file uses Selelinum to automate the process of recording raw network traffic from the instagram post 
+# comments request to parse them later
 PATH = "C:\chromedriver.exe"
 chrome_opt = webdriver.ChromeOptions()
 chrome_opt.add_argument("--auto-open-devtools-for-tabs")
@@ -32,10 +37,25 @@ notnow2 = driver.find_element_by_xpath("//button[contains(text(), 'Ahora no')]")
 time.sleep(5)
 driver.get('https://www.instagram.com/p/B166OkVBPJR/')
 #click on More Comments button
-time.sleep(5)
+time.sleep(10)
 b=driver.find_element(By.CSS_SELECTOR,"svg[aria-label='Cargar más comentarios']")
-b.click()
+while b:
+    childComments = driver.find_elements(By.XPATH,"//span[contains(text(), 'Ver respuestas ')]")
+    for c in childComments:
+        time.sleep(3)
+        c.click()
+    childComments = None
+    time.sleep(5)
+    b.click()
+    time.sleep(10)
+    try:
+        b=driver.find_element(By.CSS_SELECTOR,"svg[aria-label='Cargar más comentarios']")
+    except NoSuchElementException:
+        b=None
+else:
+    print("No more comments to load...")
 
 #Close Browser
 time.sleep(5)
-driver.close()
+print("Please download .har file from Chrome dev tools window and parse it into a json file with all the comments for the post...")
+##driver.close()
